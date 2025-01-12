2abd1469557d136bef559ad186e4f1ade1b10d98
Avijit Maity <avisrabon22@gmail.com>
2025-01-12 23:49:07 +0530

    update on pages to setup look

--- src/Component/NavBars/TopBar.jsx
+++ src/Component/NavBars/TopBar.jsx
@@ -6,9 +6,9 @@ const TopBar=()=>{
             <div className="container mx-auto flex justify-between items-center">
                 <div className="text-white text-lg font-bold">IIMI Log Tracker</div>
                 <nav className="flex space-x-4">
-                    <NavLink to="/" className="text-gray-300 hover:text-white" activeClassName="text-white">Home</NavLink>
-                    <NavLink to="/about" className="text-gray-300 hover:text-white" activeClassName="text-white">About</NavLink>
-                    <NavLink to="/contact" className="text-gray-300 hover:text-white" activeClassName="text-white">Contact</NavLink>
+                    <NavLink to="/" className="text-gray-300 hover:text-white" >Home</NavLink>
+                    <NavLink to="/about" className="text-gray-300 hover:text-white" >About</NavLink>
+                    <NavLink to="/contact" className="text-gray-300 hover:text-white" >Contact</NavLink>
                 </nav>
             </div>
         </div>
--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,13 @@ import './index.css'
 import App from './App.jsx'
 import { createBrowserRouter, createRoutesFromElements, Route,RouterProvider } from 'react-router-dom'
 import LogBoard from './Component/Production/LogBoard.jsx'
+import NotFoundPage from './Component/NotFoundPage.jsx'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
-      <Route path='' element={LogBoard}/>
+      <Route path="" element={<LogBoard/>}/>
+      <Route path="*" element={<NotFoundPage/>}/>
     </Route>
 
   )
