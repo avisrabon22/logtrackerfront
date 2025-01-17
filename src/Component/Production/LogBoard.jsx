7d0999f746b800272cac4cbaf085ab65f8b61a63
Avijit Maity <avisrabon22@gmail.com>
2025-01-17 22:45:23 +0530

    implementing search option on logboard, need to ork with back ned

--- src/Component/Production/LogBoard.jsx
+++ src/Component/Production/LogBoard.jsx
@@ -23,13 +23,29 @@ const LogBoard = () => {
     return (
         <div className="p-4">
             <h1 className="text-4xl font-bold text-center my-8">Welcome To IIMI</h1>
-            <table className="min-w-full bg-white border border-gray-200">
+            <div className="mb-4">
+                <input
+                    type="text"
+                    placeholder="Search by username"
+                    className="p-2 border border-gray-300 rounded"
+                    onChange={(e) => {
+                        const searchTerm = e.target.value.toLowerCase();
+                        setLogs((prevLogs) =>
+                            prevLogs.filter((log) =>
+                                log.username.toLowerCase().includes(searchTerm)
+                            )
+                        );
+                    }}
+                />
+            </div>
+            <table className="min-w-full bg-slate-50 border border-gray-200">
                 <thead>
                     <tr>
                         <th className="px-4 py-2 border border-green-800">ID</th>
                         <th className="px-4 py-2 border border-green-800">Username</th>
                         <th className="px-4 py-2 border border-green-800">Device Name</th>
                         <th className="px-4 py-2 border border-green-800">Device Type</th>
+                        <th className="px-4 py-2 border border-green-800">Log Id</th>
                         <th className="px-4 py-2 border border-green-800">Log Date</th>
                         <th className="px-4 py-2 border border-green-800">Log Time</th>
                     </tr>
@@ -41,6 +57,7 @@ const LogBoard = () => {
                             <td className="border border-black px-4 py-2 border-b">{log.username}</td>
                             <td className="border border-black px-4 py-2 border-b">{log.device_name}</td>
                             <td className="border border-black px-4 py-2 border-b">{log.device_type}</td>
+                            <td className="border border-black px-4 py-2 border-b">{log.log_id}</td>
                             <td className="border border-black px-4 py-2 border-b">{log.log_date}</td>
                             <td className="border border-black px-4 py-2 border-b">{log.log_time}</td>
                         </tr>
