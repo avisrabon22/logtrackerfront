30c6d51bb00aedb652a8422cd137bdae5e98ba14
Avijit Maity <avisrabon22@gmail.com>
2025-01-22 23:49:21 +0530

    progress on pie chart

--- src/Component/Production/Charts/PieChart.jsx
+++ src/Component/Production/Charts/PieChart.jsx
@@ -1,7 +1,76 @@
+import { useEffect, useState } from "react";
+import LogApi from "../../../Services/LogApi";
+import { Pie } from "react-chartjs-2";
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend,
+} from 'chart.js';
+import { toast } from "react-toastify";
 const PieChart = () => {
+
+    const [logs, setLogs] = useState([]);
+    // Fetch logs from the API
+    useEffect(() => {
+        const fetchLogs = async () => {
+            try {
+                const response = await LogApi.getLogs();
+                if (response.data.length === 0) {
+                    toast.error("No logs found", { autoClose: 1500 });
+                }
+
+
+                const logCounts = {};
+                response.data.forEach(log => {
+                    logCounts[log.id] = (logCounts[log.id] || 0) + 1;
+                });
+
+                const logIds = Object.keys(logCounts);
+                const frequencies = Object.values(logCounts);
+
+                setLogs({
+                    labels: logIds,
+                    datasets: [
+                        {
+                            data: frequencies,
+                            backgroundColor: [
+                                'rgba(255, 99, 132, 0.8)',
+                                'rgba(54, 162, 235, 0.8)',
+                                'rgba(255, 206, 86, 0.8)',
+                                'rgba(75, 192, 192, 0.8)',
+                                'rgba(153, 102, 255, 0.8)',
+                                'rgba(255, 159, 64, 0.8)',
+                            ],
+                        },
+                    ],
+                });
+            } catch (error) {
+                toast.error("Error in fetching logs", { autoClose: 1500 });
+            }
+        };
+        fetchLogs();
+    }, []);
+
+    ChartJS.register(
+        CategoryScale,
+        LinearScale,
+        PointElement,
+        LineElement,
+        Title,
+        Tooltip,
+        Legend
+    );
+
+
     return (
         <div>
-               <h1>Logs Pie</h1>
+            <h1>Logs Pie</h1>
+            <Pie  data={logs} />
         </div>
 
     );
